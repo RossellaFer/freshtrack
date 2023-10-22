db81d5e015e40ac3be37cb9efd9575e86f503b32
Emi <emi.katsuta@gmail.com>
2023-10-21 22:55:49 -0400

    Clean up

--- backend/freshtrack/validations.py
+++ backend/freshtrack/validations.py
@@ -6,13 +6,14 @@ def custom_validation(data):
     email = data['email'].strip()
     username = data['username'].strip()
     password = data['password'].strip()
-    ##
+
     if not email or UserModel.objects.filter(email=email).exists():
-        raise ValidationError('choose another email')
-    ##
+        raise ValidationError('Please choose another email.')
+
     if not password or len(password) < 8:
-        raise ValidationError('choose another password, min 8 characters')
-    ##
+        raise ValidationError('Password is too short. Minimum of 8 characaters.')
+
     if not username:
-        raise ValidationError('choose another username')
+        raise ValidationError('Username is required.')
+
     return data
